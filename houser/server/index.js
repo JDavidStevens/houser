cd7a07d9c46c7e0f0bf25ecd666b59072f60a905
unknown <jdavid.stevens@gmail.com>
2018-07-20 10:35:32 -0600

    housersetupcmt

---
+++ houser/server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const controller = require('./controller');
+const massive = require('massive');
+require('dotenv').config();
+
+const app = express();
+
+app.use(bodyParser.json());
+massive(process.env.CONNECTION_STRING)
+  .then(dbInstance => {
+    app.set('db', dbInstance);
+  })
+  .catch(err => console.log(err));
+
+const port = 4000;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}.`);
+});
